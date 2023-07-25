7a263c0046616497f490651de69be282d86c5970
aminet2a <aminetbo@gmail.com>
2023-07-26 00:27:17 +0100

    add common module

---
+++ common/src/main/java/ezpay/io/App.java
@@ -0,0 +1,8 @@
+package ezpay.io;
+
+import org.springframework.boot.SpringApplication;
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
