a37ad76352aa53ccef36d743c05537fc72e4bde5
aminet2a <aminetbo@gmail.com>
2023-07-25 05:21:21 +0100

    add Google's Jib plugin

---
+++ security-common/src/main/java/ezpay/io/securitycommon/SecurityCommonApplication.java
@@ -0,0 +1,9 @@
+package ezpay.io.securitycommon;
+
+import org.springframework.boot.SpringApplication;
+
+public class SecurityCommonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityCommonApplication.class, args);
+    }
+}
